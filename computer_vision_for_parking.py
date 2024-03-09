0808b3d05c783a973b5a5075c68365745ee4167d
elum0003 <elum0003@student.monash.edu>
2024-03-09 22:05:05 +1100

    real time detection working fine

--- computer_vision_for_parking.py
+++ computer_vision_for_parking.py
@@ -10,27 +10,36 @@ connected_components = cv2.connectedComponentsWithStats(mask,4,cv2.CV_32S)
     
 parking_spots = get_parking_spots_bboxes(connected_components)
 
-print(parking_spots[0])
-
+status_f_spots = [None for spot in parking_spots]
+step = 30
+frame_no = 0
 ret= True
 while ret:
     ret, frame = captured.read()
-    for place in parking_spots:
 
-        x1, y1, w ,h =place
+    if frame_no % step == 0:
+        for p_index, place in enumerate(parking_spots):
 
-        cropped_spot = frame[y1:y1 + h, x1:x1 + w, :]
+            x1, y1, w ,h =place
 
-        status = empty_or_not(cropped_spot)
+            cropped_spot = frame[y1:y1 + h, x1:x1 + w, :]
 
-        if status:
-            frame = cv2.rectagle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
-        frame = cv2.rectangle(frame, (x1,y1), (x1 + w, y1+ h), (255, 0, 0), 2)
+            status = empty_or_not(cropped_spot)
+            status_f_spots[p_index] = status
     
+    for p_index, place in enumerate(parking_spots):
+        status = status_f_spots[p_index]
+        x1, y1, w ,h = parking_spots[p_index]
+        if status:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+        else:
+            frame = cv2.rectangle(frame, (x1,y1), (x1 + w, y1+ h), (255, 0, 0), 2)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+    frame_no += 1
 
 captured.release()
 
